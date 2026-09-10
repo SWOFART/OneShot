08f2c11efd4a40263760fe6a3a98407c19cc521a
selezenart <artvs14@gmail.com>
2026-09-10 18:40:47 +0200

    feat(brand): derive the diagonal hero cut from the box size

--- packages/brand/src/index.ts
+++ packages/brand/src/index.ts
@@ -1,1 +1,8 @@
 export { CommitRing, type CommitRingProps } from './CommitRing.js';
+export {
+  heroClipPaths,
+  HERO_CHANNEL,
+  HERO_MIN_WIDTH,
+  HERO_RADIUS,
+  type HeroClipPaths,
+} from './heroCut.js';
